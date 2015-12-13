a6d934a886e260684c1b64a7f936dd1fae5cba73
Harsh Sheth <harsheth96@gmail.com>
2015-12-13 20:16:17 +0530

    Make something that works

--- project/penpals/views.py
+++ project/penpals/views.py
@@ -3,23 +3,23 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from .model import Applicant
+#from .models import Applicant
 
 # Create your views here.
 
 def register(request):
-	template = loader.get_template('penpals/register.html')
-	context = RequestContext
+	template = loader.get_template('register.html')
+	return HttpResponse(template.render({}))
 
 def result(request):
 	try:
 		#Get all data treating request like a POST req using request.POST(stuff). If it fails its a GET req
 		#and will go into the except block of code else it will go to the else part of it.
+		random = request.POST("random")
 	except:
 		#display get request result
-		template = loader.get_template('penpals/result.html')
-		context = RequestContext("""Whatever you need""")
-		return HttpResponse(template.render(context))
+		template = loader.get_template('result.html')
+		return HttpResponse(template.render({}))
 	else:
 		#add entry to db
 		return HttpResponseRedirect(reverse('result')) #Redirect to itself
