e8e105dd8fa5946dcc5a50d360ef24100b521427
Harsh Sheth <harsheth96@gmail.com>
2015-12-18 00:29:49 +0530

    Decorate stuff

--- project/penpals/urls.py
+++ project/penpals/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.register, name = "registerform"),
+    url(r'^/$', views.register, name = "registerform"),
 	url(r'^register/$', views.register, name = 'registerform'),
 	url(r'^result/$', views.result, name = 'result'),
     url(r'^view/$', views.view, name = 'view'),
